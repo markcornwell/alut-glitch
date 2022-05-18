dd11f06185d0432772ebbae4e530fb351ee814e5
Mark Cornwell <mark@cornwellgamelab.com>
2022-05-17 23:40:11 -0400

    second commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for alut-glitch
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,25 @@
+{- Demonstrate build issues with Sound.ALUT on Windows 
+credit to https://bluishcoder.co.nz/articles/haskell/openal.html
+-}
+
+import Control.Monad
+import Sound.ALUT
+
+playSound :: IO ()
+playSound =
+  withProgNameAndArgs runALUTUsingCurrentContext $ \_ _ ->
+  do
+    (Just device) <- openDevice Nothing
+    (Just context) <- createContext device []
+    currentContext $= Just context
+    buffer1 <- createBuffer $ Sine 440 0 1
+    buffer2 <- createBuffer HelloWorld
+    [source] <- genObjectNames 1
+    queueBuffers source [buffer1,buffer2]
+    play [source]
+    sleep 4
+    closeDevice device
+    return ()
+
+main = playSound
+
